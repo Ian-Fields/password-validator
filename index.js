e5587734fc3cb895b2f593986ece341fc7764afc
Ian Fields <ianfields@Ians-Air.fios-router.home>
2020-03-24 21:42:10 -0400

    created if statements to solve each requirement, tried making as clean as possible. used es6 in the beginning but went back to ensure all the code was uniform. attempted regex but will save that for another day lol

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-function validatePassword() {
-
+function validatePassword(valid) {
+  if (passwordLength(valid) === true && passwordLowerCase(valid) === true && passwordUpperCase(valid) === true && passwordNumericValue(valid) === true && passwordSpecialCharacter(valid) === true) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+function passwordLength(valid) {
+  if (valid.length >= 8) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+function passwordLowerCase(valid) {
+  if (valid.match(/[a-z]/)) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+function passwordUpperCase(valid) {
+  if (valid.match(/[A-Z]/)) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+function passwordNumericValue(valid) {
+  if (valid.match(/[0-9]/)) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+function passwordSpecialCharacter(valid) {
+  if (valid.match(/[!@#$%^&*(),.?"]/)) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 module.exports = validatePassword
